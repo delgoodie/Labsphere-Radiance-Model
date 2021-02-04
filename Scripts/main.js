e1a8d73c888a9788d8a204865a0fdcf0924bd8a0
William Del Giudice <delwillgiudice@gmail.com>
2021-02-04 13:34:29 -0500

    last commit before server resources loaded

--- Scripts/main.js
+++ Scripts/main.js
@@ -23,3 +23,16 @@ function CreateElement(_type, _parent, _class, _text) {
     if (_parent) $(_parent).append(ret);
     return ret;
 }
+
+/*
+Object.getOwnPropertyNames(LampData).forEach((n, i) => {
+    if (i != 28) return;
+    let flux = {};
+    FluxData[n].forEach((f, i) => flux[i + 250] = f);
+    var json = JSON.stringify(flux);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'http://localhost:3000/lamp?name=' + n + '&portDiameter=' + LampData[n].portDiameter + '&vaa=' + LampData[n].vaa + '&type=' + LampData[n].type + '&power=' + LampData[n].power + '&voltage=' + LampData[n].voltage);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    console.log(xhr.send(json));
+});
+*/
